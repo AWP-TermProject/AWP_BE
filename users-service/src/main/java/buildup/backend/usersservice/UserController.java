1457ba0836834e0339915cff4b48a2981e38ebcf
jangwon <wkddnjs9898@gmail.com>
2023-05-28 00:47:46 +0900

    user service database backend

---
+++ users-service/src/main/java/buildup/backend/usersservice/UserController.java
@@ -0,0 +1,42 @@
+package buildup.backend.usersservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+//@CrossOrigin("http://localhost:8083")
+@RestController
+public class UserController {
+    @Autowired
+    private final UserRepository userRepository;
+    public UserController(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+    @GetMapping("/api/user/{id}")
+    public Optional<User> getMemberId(@PathVariable("id") Long id ){
+        return  userRepository.findById(id);
+    }
+
+    @GetMapping("/")
+    public User  index(){
+        User member = new User(1, "gachon", "gachon.ac.kr", "gcu");
+        return member;
+    }
+    @GetMapping("/api/user")
+    public List<User> showAll(){
+        return userRepository.findAll();
+    }
+    @PostMapping("/api/user")
+    public String create(@RequestBody User member) {
+        userRepository.save(member);
+        return "member create ok";
+    }
+
+    //회원가입 api : post/api/register/local
+
+    //로그인 api : post /api/login/local
+
+    //이메일 아이디 중복확인 GET /exists/:key/
+}
